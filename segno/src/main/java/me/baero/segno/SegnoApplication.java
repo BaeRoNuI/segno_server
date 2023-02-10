2764ef797f2cacbc6b9e02c9111f66484e85d342
BaeRoNuI <xogh20321@gmail.com>
2023-02-11 00:50:24 +0900

    initial commit

---
+++ segno/src/main/java/me/baero/segno/SegnoApplication.java
@@ -0,0 +1,13 @@
+package me.baero.segno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegnoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SegnoApplication.class, args);
+    }
+
+}
---
+++ segno/src/test/java/me/baero/segno/SegnoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.baero.segno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegnoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
